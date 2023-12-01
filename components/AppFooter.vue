6835e7c9756c7883953898cb157d6b071e1e82a1
kelvin Mutuota <kelvinmutuota749@gmail.com>
2023-12-01 12:20:49 +0300

    feat(app): app footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-green-500 h-[120x]">
+        <div class="flex justify-between">
+            <div class="h-[100px] bg-gray-400 w-[30px]"></div>
+            <div class="h-[100px] bg-gray-400 w-[30px]"></div>
+            <div class="h-[100px] bg-gray-400 w-[30px]"></div>
+            <div class="h-[100px] bg-gray-400 w-[30px]"></div>
+        </div>
+        <div><p>Copyright</p></div>
+    </div>
+</template>
